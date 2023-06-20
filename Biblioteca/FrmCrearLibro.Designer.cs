250e4ba3be5275068ea7b662aea08807059ef21b
Hristo Chavez <hristoandychavezhuertas@outlook.com>
2023-06-19 23:37:47 -0500

    Controles para el formulario de registro de libro

--- Biblioteca/FrmCrearLibro.Designer.cs
+++ Biblioteca/FrmCrearLibro.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            btnRegister = new Button();
+            txtTitle = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(54, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre de libro:";
+            // 
+            // btnRegister
+            // 
+            btnRegister.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRegister.Location = new Point(435, 248);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(99, 43);
+            btnRegister.TabIndex = 1;
+            btnRegister.Text = "Registrar";
+            btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(199, 69);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(161, 23);
+            txtTitle.TabIndex = 2;
+            // 
+            // FrmCrearLibro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(568, 326);
+            Controls.Add(txtTitle);
+            Controls.Add(btnRegister);
+            Controls.Add(label1);
+            Name = "FrmCrearLibro";
+            Text = "Registrar libro";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button btnRegister;
+        private TextBox txtTitle;
     }
 }
