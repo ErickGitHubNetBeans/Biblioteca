d4313122e659b02f38a07e9da66d83c6a550d85e
Hristo Chavez <hristoandychavezhuertas@outlook.com>
2023-06-21 01:22:08 -0500

    Agregando métodos y corrigiendo atributos

--- Biblioteca/Libro.cs
+++ Biblioteca/Libro.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Permissions;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms.VisualStyles;
-
-namespace Biblioteca
+﻿namespace Biblioteca
 {
     internal class Libro
     {
         //Atributos:
-        private int idLibro;
+        private int id;
         private string titulo;
         private string autor;
         private int anio;
         private bool estadoLibro; //disponible o no disponible
 
         //Constructor:
-        Libro()
+        public Libro(int id, string titulo, string autor, int anio)
         {
-
+            this.id = id;
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anio = anio;
         }
         //Métodos de acceso:
-        public void setIdLibro(int id)
+        public void setId(int id)
         {
-            this.idLibro = id;
+            this.id = id;
         }
-        public int getIdLibro()
+        public int getId()
         {
-            return this.idLibro;
+            return this.id;
         }
         public void setTitulo(string titulo)
         {
@@ -56,13 +50,56 @@ namespace Biblioteca
         {
             return this.anio;
         }
-        public void setEstadoLibro(bool estadoLibro)
+        public void setEstado(bool estadoLibro)
         {
             this.estadoLibro= estadoLibro;
         }
-        public bool getEstadoLibro()
+        public bool getEstado()
         {
             return this.estadoLibro;
+        }
+
+        // Compara los id de dos objetos. Útil para saber si un nodo es igual
+        // a otro que pretende ingresar al ABB.       
+        public int compare(Libro l)
+        {
+            if (this.getId() == l.getId())
+            {
+                return 0;
+            }
+            else if (this.getId() < l.getId())
+            {
+                // El elemento que pretende ingresar al ABB estaria
+                // ingresando por la derecha.
+                return 1;
+            }
+            else
+            {
+                // El elemento que pretende ingresar al ABB estaria
+                // ingresando por la izquierda.
+                return -1;
+            }
+        }
+
+        // Valida si existe un identificador de objeto en el árbol.
+        public int compareId(int id)
+        {
+            if (this.getId() == id)
+            {
+                return 0;
+            }
+            else if (this.getId() < id)
+            {
+                // El elemento que pretende ingresar al ABB estaria
+                // ingresando por la derecha.
+                return 1;
+            }
+            else
+            {
+                // El elemento que pretende ingresar al ABB estaria
+                // ingresando por la izquierda.
+                return -1;
+            }
         }
 
         //METODOS CRUD
@@ -73,4 +110,3 @@ namespace Biblioteca
         public void eliminarLibro() { }
     }
 }
-
