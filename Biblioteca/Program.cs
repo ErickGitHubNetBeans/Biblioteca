ad0e222b898b6f95dfb28e1e0d2be062763b15ba
Hristo Chavez <hristoandychavezhuertas@outlook.com>
2023-06-22 01:05:01 -0500

    Configuraciones para ejecutar el proyecto

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -11,7 +11,9 @@ namespace Biblioteca
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmLogin());
+            frmLogin frmLogin = new frmLogin();
+            frmLogin.Show();
+            Application.Run();
         }
     }
 }
