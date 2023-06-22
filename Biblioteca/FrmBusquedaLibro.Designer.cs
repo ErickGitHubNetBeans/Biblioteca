c259bea70e677043d1b48e905bebe5e6575bdf2b
Hristo Chavez <hristoandychavezhuertas@outlook.com>
2023-06-22 00:58:38 -0500

    Formulario para busqueda de libro

---
+++ Biblioteca/FrmBusquedaLibro.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Biblioteca
+{
+    partial class FrmBusquedaLibro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(37, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(222, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Busqueda de libro";
+            // 
+            // FrmBusquedaLibro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(299, 151);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            Name = "FrmBusquedaLibro";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Buscar libro";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
