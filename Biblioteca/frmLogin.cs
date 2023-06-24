d594a42addb60b75b3a573d4de94d7c0c043277b
Hristo Chavez <hristoandychavezhuertas@outlook.com>
2023-06-24 16:55:06 -0500

    Ruta y ABB accesible durante todo el tiempo de ejecución

--- Biblioteca/frmLogin.cs
+++ Biblioteca/frmLogin.cs
@@ -7,14 +7,27 @@ namespace Biblioteca
             InitializeComponent();
         }
 
-
         private void btnIngresar_Click(object sender, EventArgs e)
         {
             //usuario Admin, contrasenia 12345
             if (txtUsuario.Text.Equals("admin") &&
                 txtContrasenia.Text.Equals("12345"))
             {
-                FrmMenuPrincipal menuPrincipal = new FrmMenuPrincipal();
+                // Ruta de la carpeta.
+                string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Nombre del archivo.
+                string filename = "libros.txt";
+
+                // Ruta completa del archivo.
+                string fullPath = dir + "\\" + filename;
+
+                // Nuevo árbol binario de busqueda accesible a lo largo 
+                // de toda la aplicación mientras está en ejecución.
+                ABB abb = new ABB();
+
+                FrmMenuPrincipal menuPrincipal = 
+                    new FrmMenuPrincipal(fullPath, abb);
                 this.Hide();
                 menuPrincipal.Show();
             }
